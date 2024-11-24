241fa68d65ddd2e43a4842a4d3fc959104dba4cb
kobirul5 <kobirul05j@gmail.com>
2024-11-24 13:08:24 +0600

    send data and get data and show in wesite

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,10 @@ function App() {
     .then(res=>res.json())
     .then(data=>{
       console.log("inside post response",data)
+      const newUsers = [...users, data]
+      setUsers(newUsers)
     })
+    // form.reset()
   }
 
 
