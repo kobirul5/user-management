dde502b4d1895616764a96700c0b7143288f69c3
kobirul5 <kobirul05j@gmail.com>
2024-11-24 12:52:39 +0600

    user Managemenet

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+
+import { useState } from 'react'
+import './App.css'
+import { useEffect } from 'react'
+
+function App() {
+  const [users, setUsers] = useState([])
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then((res)=>res.json())
+    .then(data=>setUsers(data))
+  },[])
+
+  const handleSubmit = e =>{
+    e.preventDefault()
+    const form = e.target
+    const name = form.name.value
+    const email = form.email.value
+    const user = {name, email}
+    console.log(user)
+    fetch('http://localhost:5000/users',{
+      method: 'POST',
+      headers: {
+        'content-type':'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log("inside post response",data)
+    })
+  }
+
+
+  return (
+    <>
+      <h1>User Management </h1>
+      <h1>User Management {users.length} </h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="" /> <br />
+        <input type="email" name="email" id="" /> <br />
+        <input type="submit" value="submit" />
+      </form>
+
+      {
+        users.map(user=> <p key={user.id}>{user.id}: {user.name}: {user.email} </p>)
+      }
+
+    </>
+  )
+}
+
+export default App
